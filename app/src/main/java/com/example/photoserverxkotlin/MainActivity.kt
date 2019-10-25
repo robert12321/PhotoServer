85c5942cb255fa5db201f3156c8907f83802cb05
robert12321 <roberto12321@tlen.pl>
2019-10-25 21:56:02 +0200

    Signed-off-by: robert12321 <roberto12321@tlen.pl>

--- app/src/main/java/com/example/photoserverxkotlin/MainActivity.kt
+++ app/src/main/java/com/example/photoserverxkotlin/MainActivity.kt
@@ -20,6 +20,15 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import java.io.File
 import java.util.concurrent.Executors
+import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Handler
+import androidx.core.os.postDelayed
+import java.nio.ByteBuffer
+import java.util.concurrent.TimeUnit
+
 
 // This is an arbitrary number we are using to keep track of the permission
 // request. Where an app has multiple context for requesting permission,
@@ -48,12 +57,12 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
             updateTransform()
         }
+
     }
     // Add this after onCreate
 
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var viewFinder: TextureView
-
     private fun startCamera() {
         // TODO: Implement CameraX operations
         // Create configuration object for the viewfinder use case
@@ -118,11 +127,27 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
                 })
         }
 
+        // Add this before CameraX.bindToLifecycle
+
+        // Setup image analysis pipeline that computes average pixel luminance
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
+
+        // Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, LuminosityAnalyzer())
+        }
+
         // Bind use cases to lifecycle
         // If Android Studio complains about "this" being not a LifecycleOwner
         // try rebuilding the project or updating the appcompat dependency to
         // version 1.1.0 or higher.
-        CameraX.bindToLifecycle(this, preview, imageCapture)
+        CameraX.bindToLifecycle(
+            this, preview, imageCapture, analyzerUseCase)
     }
 
     private fun updateTransform() {
@@ -146,6 +171,44 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         // Finally, apply transformations to our TextureView
         viewFinder.setTransform(matrix)
     }
+
+    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        /**
+         * Helper extension function used to extract a byte array from an
+         * image plane buffer
+         */
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+            val currentTimestamp = System.currentTimeMillis()
+            // Calculate the average luma no more often than every second
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                TimeUnit.SECONDS.toMillis(1)) {
+                // Since format in ImageAnalysis is YUV, image.planes[0]
+                // contains the Y (luminance) plane
+                val buffer = image.planes[0].buffer
+                // Extract image data from callback object
+                val data = buffer.toByteArray()
+                // Convert the data into an array of pixel values
+                val pixels = data.map { it.toInt() and 0xFF }
+                // Compute average luminance for the image
+                val luma = pixels.average()
+                // Log the new luma value
+                Log.d("CameraXApp", "Average luminosity: $luma")
+                // Update timestamp of last analyzed frame
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+        }
+    }
+
+
 
     /**
      * Process result from permission request dialog box, has the request
